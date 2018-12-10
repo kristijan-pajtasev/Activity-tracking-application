ff781c49bc759947489904177e90a42dbdaa85b8
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-12-10 22:34:12 +0000

    refactor

--- app/src/main/java/com/kristijan_pajtasev/assignment03/LocationPointUtil.java
+++ app/src/main/java/com/kristijan_pajtasev/assignment03/LocationPointUtil.java
@@ -44,4 +44,45 @@ public class LocationPointUtil {
         }
         return loss;
     }
+
+    public static double maxSpeed(ArrayList<LocationPoint> locationPoints) {
+        double maxSpeed = 0;
+        for(int i = 0; i < locationPoints.size() - 1; i++) {
+            LocationPoint current = locationPoints.get(i);
+            LocationPoint next = locationPoints.get(i + 1);
+
+            double speed = distanceBetweenPoints(current, next) / 5;
+
+            if(speed > maxSpeed) maxSpeed = speed;
+        }
+        return maxSpeed;
+    }
+
+    public static double minSpeed(ArrayList<LocationPoint> locationPoints) {
+        double minSpeed = 0;
+        for(int i = 0; i < locationPoints.size() - 1; i++) {
+            LocationPoint current = locationPoints.get(i);
+            LocationPoint next = locationPoints.get(i + 1);
+
+            double speed = distanceBetweenPoints(current, next) / 5;
+            if(speed < minSpeed || i == 0) minSpeed = speed;
+        }
+        return minSpeed;
+    }
+
+    private static double distanceBetweenPoints(LocationPoint point1, LocationPoint point2) {
+        double x1 = point1.getAltitude() * Math.cos(point1.getLatitude()) * Math.sin(point1.getLongitude());
+        double y1 = point1.getAltitude() * Math.sin(point1.getLatitude());
+        double z1 = point1.getAltitude() * Math.cos(point1.getLatitude()) * Math.cos(point1.getLongitude());
+
+        double x2 = point2.getAltitude() * Math.cos(point2.getLatitude()) * Math.sin(point2.getLongitude());
+        double y2 = point2.getAltitude() * Math.sin(point2.getLatitude());
+        double z2 = point2.getAltitude() * Math.cos(point2.getLatitude()) * Math.cos(point2.getLongitude());
+
+        return Math.sqrt(
+                Math.pow(x2 - x1, 2) +
+                        Math.pow(y2 - y1, 2) +
+                        Math.pow(z2 - z1, 2)
+        );
+    }
 }
--- app/src/main/java/com/kristijan_pajtasev/assignment03/StatisticsActivity.java
+++ app/src/main/java/com/kristijan_pajtasev/assignment03/StatisticsActivity.java
@@ -19,10 +19,14 @@ public class StatisticsActivity extends Activity {
         double minAltitude = LocationPointUtil.minAltitude(locationPoints);
         double altitudeGain = LocationPointUtil.altitudeGain(locationPoints);
         double altitudeLoss = LocationPointUtil.altitudeLoss(locationPoints);
+        double maxSpeed = LocationPointUtil.maxSpeed(locationPoints);
+        double minSpeed = LocationPointUtil.minSpeed(locationPoints);
 
         ((TextView)findViewById(R.id.minAltitude)).setText(minAltitude + "");
         ((TextView)findViewById(R.id.maxAltitude)).setText(maxAltitude + "");
         ((TextView)findViewById(R.id.altitudeGain)).setText(altitudeGain + "");
         ((TextView)findViewById(R.id.altitudeLoss)).setText(altitudeLoss + "");
+        ((TextView)findViewById(R.id.maxSpeed)).setText(maxSpeed + "");
+        ((TextView)findViewById(R.id.minSpeed)).setText(minSpeed + "");
     }
 }
