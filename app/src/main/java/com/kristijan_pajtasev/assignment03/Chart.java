cce698ffb6a3c49196c27700f77293fbd507278a
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-12-10 23:48:51 +0000

    init custom view

---
+++ app/src/main/java/com/kristijan_pajtasev/assignment03/Chart.java
@@ -0,0 +1,20 @@
+package com.kristijan_pajtasev.assignment03;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Chart extends View {
+    public Chart(Context context) {
+        super(context);
+    }
+
+    public Chart(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Chart(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
